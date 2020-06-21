eeb27d8773700ed2983dd3789a45173a44f06851
TheMshary <themshary94@gmail.com>
2020-06-21 14:19:00 +0300

    First commit

--- story.js
+++ story.js
@@ -1,7 +1,12 @@
 // Define the name here on line 2
-
+const name = "Mshary";
 // Define the age here on line 4
-
+const age = 25;
 // Define the hobby here on line 6
-
+const hobby = "Watching Anime";
 // Print the story here on line 8
+console.log(
+  `Mr./Mrs. ${name}, in 33 years, you will be ${
+    age + 33
+  } years old. By then you'll have decades of ${hobby} practice!`
+);
