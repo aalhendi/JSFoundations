32469adccbe3cd3c31f76a47a28dae0c663e902a
TheMshary <themshary94@gmail.com>
2020-06-21 14:07:21 +0300

    Split tests into two tests

--- tests/project.spec.js
+++ tests/project.spec.js
@@ -16,9 +16,7 @@ import {
   isUpperCase,
   elementInArray,
   reverseString,
-} from "./assessment";
-
-import { generateIntegersArray, noZeroes } from "./debug-assessment";
+} from "../project";
 
 describe("sumOdds(numbers)", () => {
   test("returns the sum of only the ODD numbers.", () => {
@@ -114,28 +112,5 @@ describe("reverseString(string)", () => {
     const string = "rfvwkev";
     const reversed = "vekwvfr";
     expect(reverseString(string)).toEqual(reversed);
-  });
-});
-
-describe("generateIntegersArray(firstNumber, lastNumber)", () => {
-  test("returns an array of numbers between the two numbers passed as parameters.", () => {
-    const firstNumber = 4;
-    const lastNumber = 13;
-    const res = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
-    expect(generateIntegersArray(firstNumber, lastNumber)).toEqual(res);
-  });
-});
-
-describe("noZeroes(numberString)", () => {
-  test("doesn't remove zeros in the middle.", () => {
-    const numberString = "0010100";
-    const zeroless = "101";
-    expect(noZeroes(numberString)).toEqual(zeroless);
-  });
-
-  test("returns the number without zeroes at the end or the beginning.", () => {
-    const numberString = "00100";
-    const zeroless = "1";
-    expect(noZeroes(numberString)).toEqual(zeroless);
   });
 });
