2c253faa2f49384f7854e45144de3192bd8daa71
theHasanas <30086691+theHasanas@users.noreply.github.com>
2020-10-20 14:36:44 +0300

    Initial commit

---
+++ arrayFunctions.js
@@ -0,0 +1,77 @@
+/**
+ * oddOrEven(a):
+ * - receives array `a`
+ * - returns "odd" if array has odd number of elements
+ * - returns "even" if array has even number of elements
+ *
+ * e.g.
+ * oddOrEven([1, 2, 3]) -> "odd"
+ * oddOrEven([1, 2, 3, 4]) -> "even"
+ */
+function oddOrEven(a) {
+  // Your code here
+}
+
+/**
+ * addLailaToArray(a):
+ * - receives array `a`
+ * - returns a new array that's a copy of array `a` with additional string "Laila"
+ *
+ * e.g.
+ * addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
+ */
+function addLailaToArray(a) {
+  // Your code here
+}
+
+/**
+ * eliminateTeam(a):
+ * - receives array `a`
+ * - removes the last element from the array and return it
+ *
+ * e.g.
+ * eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
+ */
+function eliminateTeam(a) {
+  // Your code here
+}
+
+/**
+ * secondHalfOfArrayIfItIsEven(a):
+ * - receives array `a`
+ * - returns a new array that's the second half of the original array if it has an even number of elements
+ * - returns an empty array if it has an odd number of elements
+ *
+ * e.g.
+ * secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi"]) -> ["banana", "kiwi"]
+ * secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi", "blueberry"]) -> []
+ */
+function secondHalfOfArrayIfItIsEven(a) {
+  // Your code here
+}
+
+/**
+ * youGottaCalmDown(s):
+ * - receives a string `s`
+ * - returns the string `s` with at most one exclamation mark (!) at the end.
+ *
+ * e.g.
+ * youGottaCalmDown("HI!!!!!!!!!!") -> "HI!"
+ * youGottaCalmDown("Taylor Schwifting!!!!!!!!!!!") -> "Taylor Shwifting!"
+ * youGottaCalmDown("Hellooooo") -> "Hellooooo"
+ *
+ * Hint:
+ * - Use string method .slice()
+ * - Use string method .endsWith()
+ */
+function youGottaCalmDown(s) {
+  // Your code here
+}
+
+module.exports = {
+  oddOrEven,
+  addLailaToArray,
+  eliminateTeam,
+  secondHalfOfArrayIfItIsEven,
+  youGottaCalmDown,
+};
