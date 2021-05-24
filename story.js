26f1f6f7b94e02d2c6b38e47353938eecd0c70b0
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2021-05-24 16:30:49 +0300

    Added story.js

---
+++ story.js
@@ -0,0 +1,7 @@
+let name = "Abdulrazzaq";
+let age = 22;
+let hobby = "debugging";
+
+console.log(
+    `Mr. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`
+);
