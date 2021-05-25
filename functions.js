1d8be058a1f7375cdb7003293f6d290c7917b5fa
Abdulrazzaq Alhendi <abdulrazaq.alhendi@gmail.com>
2021-05-25 17:21:54 +0300

    Completed Task

--- functions.js
+++ functions.js
@@ -7,7 +7,7 @@
  * greet("Hamza") logs "Hello Hamza"
  */
 function greet(name) {
-  // Your code here
+    console.log(`Hello ${name}`);
 }
 
 /**
@@ -20,7 +20,11 @@ function greet(name) {
  * isOdd(10) -> false
  */
 function isOdd(n) {
-  // Your code here
+    if (n % 2 === 1) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 /**
@@ -33,7 +37,11 @@ function isOdd(n) {
  * oddsSmallerThan(15) -> 7
  */
 function oddsSmallerThan(n) {
-  // Your code here
+    if (isOdd(n)) {
+        return (n - 1) / 2;
+    } else {
+        return n / 2;
+    }
 }
 
 /**
@@ -47,7 +55,11 @@ function oddsSmallerThan(n) {
  * squareOrDouble(9) -> 81
  */
 function squareOrDouble(n) {
-  // Your code here
+    if (isOdd(n)) {
+        return n * n;
+    } else {
+        return n * 2;
+    }
 }
 
 module.exports = { greet, isOdd, oddsSmallerThan, squareOrDouble };
